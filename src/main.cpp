cd3f76db9556d5e091cc833c1bdc7d7b76991803
rein0925 <mo.kensei0925@gmail.com>
2023-08-09 12:02:31 +0900

    main.cppを修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,70 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <PS4Controller.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  PS4.begin("b8:d6:1a:bc:e6:a2");
+  Serial.println("Ready.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Below has all accessible outputs from the controller
+  if (PS4.isConnected()) {
+    if (PS4.Right()) Serial.println("Right Button");
+    if (PS4.Down()) Serial.println("Down Button");
+    if (PS4.Up()) Serial.println("Up Button");
+    if (PS4.Left()) Serial.println("Left Button");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (PS4.Square()) Serial.println("Square Button");
+    if (PS4.Cross()) Serial.println("Cross Button");
+    if (PS4.Circle()) Serial.println("Circle Button");
+    if (PS4.Triangle()) Serial.println("Triangle Button");
+
+    if (PS4.UpRight()) Serial.println("Up Right");
+    if (PS4.DownRight()) Serial.println("Down Right");
+    if (PS4.UpLeft()) Serial.println("Up Left");
+    if (PS4.DownLeft()) Serial.println("Down Left");
+
+    if (PS4.L1()) Serial.println("L1 Button");
+    if (PS4.R1()) Serial.println("R1 Button");
+
+    if (PS4.Share()) Serial.println("Share Button");
+    if (PS4.Options()) Serial.println("Options Button");
+    if (PS4.L3()) Serial.println("L3 Button");
+    if (PS4.R3()) Serial.println("R3 Button");
+
+    if (PS4.PSButton()) Serial.println("PS Button");
+    if (PS4.Touchpad()) Serial.println("Touch Pad Button");
+
+    if (PS4.L2()) {
+      Serial.printf("L2 button at %d\n", PS4.L2Value());
+    }
+    if (PS4.R2()) {
+      Serial.printf("R2 button at %d\n", PS4.R2Value());
+    }
+
+    if (PS4.LStickX()) {
+      Serial.printf("Left Stick x at %d\n", PS4.LStickX());
+    }
+    if (PS4.LStickY()) {
+      Serial.printf("Left Stick y at %d\n", PS4.LStickY());
+    }
+    if (PS4.RStickX()) {
+      Serial.printf("Right Stick x at %d\n", PS4.RStickX());
+    }
+    if (PS4.RStickY()) {
+      Serial.printf("Right Stick y at %d\n", PS4.RStickY());
+    }
+
+    if (PS4.Charging()) Serial.println("The controller is charging");
+    if (PS4.Audio()) Serial.println("The controller has headphones attached");
+    if (PS4.Mic()) Serial.println("The controller has a mic attached");
+
+    Serial.printf("Battery Level : %d\n", PS4.Battery());
+
+    Serial.println();
+    // This delay is to make the output more human readable
+    // Remove it when you're not trying to see the output
+    delay(1000);
+  }
 }
