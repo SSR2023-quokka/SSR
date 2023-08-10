09570afa2bbbc561f916b71a227ce64376aad18e
rein0925 <mo.kensei0925@gmail.com>
2023-08-10 23:26:41 +0900

    PS4Operation.cppを修正

--- src/PS4Operation.cpp
+++ src/PS4Operation.cpp
@@ -1,12 +1,7 @@
 #include <PS4Operation.hpp>
 
 void PS4Operation() {
-  if (PS4.isConnected()) {
-    double PS4LStickDistance = sqrt(pow(PS4.LStickX(), 2) + pow(PS4.LStickY(), 2));
-    double degree = atan2(PS4.LStickY(), PS4.LStickX());
-    if (PS4LStickDistance > 70) {
-      double degree = atan2(PS4.LStickY(), PS4.LStickX());
-      Serial.printf("%lf\n", degree);
+    if (PS4.isConnected()) {
+        
     }
-  }
 }
