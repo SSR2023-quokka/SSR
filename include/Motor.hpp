f0ec70c137dcc736198d4e10bd8bade93af9031b
rein0925 <mo.kensei0925@gmail.com>
2023-08-13 02:20:15 +0900

    Motor.hppを追加

---
+++ include/Motor.hpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <AnalogOut.hpp>
+#include <DigitalOut.hpp>
+
+struct Motor {
+    int pwm;
+    int dir;
+    int dirch;
+
+    void setup();
+    void motorRun(int i);
+};
