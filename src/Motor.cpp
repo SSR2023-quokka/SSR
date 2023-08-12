6d785bbe09f58034dfc26cbe5f0657d00a366f16
rein0925 <mo.kensei0925@gmail.com>
2023-08-13 02:20:44 +0900

    Motor.cppを修正

---
+++ src/Motor.cpp
@@ -0,0 +1,14 @@
+#include <Motor.hpp>
+
+void Motor::setup() {
+    AnalogOut analogMotor{dir, dirch, 20000, 8};
+    DigitalOut digitalMotor{pwm};
+    analogMotor.setup();
+    digitalMotor.setup();
+}
+
+void Motor::motorRun(int v) {
+    int i = 128 - v;
+    digitalWrite(pwm, HIGH);
+    ledcWrite(dirch, i);
+}
