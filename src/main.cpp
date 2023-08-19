6c92b0374f5db62efdbd4ebc8b4258702fa986c7
rein0925 <mo.kensei0925@gmail.com>
2023-08-19 22:53:55 +0900

    LowerBody.hppを使用するように変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,15 @@
 #include <PS4Controller.h>
-#include "Motor.hpp"
-// #include "LowerBody.hpp"
-
-Motor motor1{0, 4, 0};  //ピンは適当
-Motor motor2{16, 17, 1};
-Motor motor3{5, 18, 2};
+#include "LowerBody.hpp"
 
 void setup() {
     Serial.begin(9600);
     PS4.begin("b8:d6:1a:bc:e6:a2");
     Serial.println("Ready.");
-    motor1.setup();
-    motor2.setup();
-    motor3.setup();
-    // lowerBodySetup();
+    lowerBodySetup();
 }
 
 void loop() {
     if (PS4.isConnected()) {
-    double degree = atan2(PS4.LStickY(), PS4.LStickX());
-    double vx = cos(degree) * 32;
-    double vy = sin(degree) * 32;
-    int v1 = -vx / 2 + vy * sqrt(3) / 2;
-    int v2 = -vx / 2 - vy * sqrt(3) / 2;
-    int v3 = vx;
-
-    double PS4LStickDistance = sqrt(pow(PS4.LStickX(), 2) + pow(PS4.LStickY(), 2));
-    if (PS4LStickDistance > 70) {
-        // Serial.printf("v1 = %d, v2 = %d, v3 = %d\n", v1, v2, v3);
-        motor1.motorRun(v1);
-        motor2.motorRun(v2);
-        motor3.motorRun(v3);
-        } else {
-        motor1.motorRun(0);
-        motor2.motorRun(0);
-        motor3.motorRun(0);
-        }
-    // lowerBody();
+        lowerBody();
     }
 }
