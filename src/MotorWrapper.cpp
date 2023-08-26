9d4fde80c370ba5342d3147f234e1cdc66e25521
rein0925 <mo.kensei0925@gmail.com>
2023-08-26 23:51:02 +0900

    filterPower を削除し FT を追加

--- src/MotorWrapper.cpp
+++ src/MotorWrapper.cpp
@@ -4,11 +4,6 @@ void MotorWrapper::motorWrapperSetup() {
     motor.setup();
 }
 
-int MotorWrapper::filterPower(int p) {
-    //ここでパワーが急激に変化しないようにする
-    return p;
-}
-
 void MotorWrapper::motorRun(int p) {
-    motor.motorRun(filterPower(p));
+    motor.motorRun(FT.filterPower(p));
 }
