85d868d5cda30748c8a45d649369068a97234c42
rein0925 <mo.kensei0925@gmail.com>
2023-09-08 18:03:34 +0900

    arm と armSetup を加えた

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <PS4Controller.h>
 #include "MachineMoving.hpp"
+#include "Arm.hpp"
 
 void setup() {
     Serial.begin(9600);
     PS4.begin("b8:d6:1a:bc:e6:a2");
     Serial.println("Ready.");
     machineMovingSetup();
+    armSetup();
 }
 
 void loop() {
     if (PS4.isConnected()) {
         machineMoving();
+        arm();
     }
 }
  
