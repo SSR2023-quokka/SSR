7b7d2cd2f2240d8e8d9e37ce0fd712ffa9c7b747
rein0925 <mo.kensei0925@gmail.com>
2023-09-11 01:15:30 +0900

    名前を分かりやすいものに変更

--- src/Arm.cpp
+++ src/Arm.cpp
@@ -2,7 +2,7 @@
 
 ServoMotor servoMotor1{32};
 
-int pos = 0;
+int pos1 = 0;
 int l = 0;
 
 void armSetup() {
@@ -11,17 +11,17 @@ void armSetup() {
 
 void arm() {
 	if (l == 0 && PS4.Triangle()) {
-    	for (pos = 0; pos <= 60; pos += 1) {
-			servoMotor1.servoWrite(pos);
-			Serial.printf("%d\n", pos);
+    	for (pos1 = 0; pos1 <= 60; pos1 += 1) {
+			servoMotor1.write(pos1);
+			Serial.printf("arm position = %d\n", pos1);
 	    	delay(30);
 		}
 		l = 1;
 	}
 	else if (l == 1 && PS4.Triangle()) {
-		for (pos = 60; pos >= 0; pos -= 1) {
-			servoMotor1.servoWrite(pos);
-			Serial.printf("%d\n", pos);
+		for (pos1 = 60; pos1 >= 0; pos1 -= 1) {
+			servoMotor1.write(pos1);
+			Serial.printf("arm position = %d\n", pos1);
 			delay(30);
 		}
 		l = 0;
