8c5a8b5264241e354bf3f344296d0f427ceb269a
rein0925 <mo.kensei0925@gmail.com>
2023-09-11 01:16:32 +0900

    コメントを追加

--- src/LineTracer.cpp
+++ src/LineTracer.cpp
@@ -5,7 +5,7 @@ void LineTracer::lineTracerSetup() {
 }
 
 bool LineTracer::detectLine() {
-        if (pin.read() < 800) {     //白
+        if (pin.read() < 800) {     //白    //値は実験で求める
             return true;
         } else {    //白以外
              return false;
